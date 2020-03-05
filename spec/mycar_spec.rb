6d77971cd56a9296f659d8bd97307d0562172664
nikolatriki <kjofte@yahoo.com>
2020-03-05 18:07:49 +0100

    changed tab spaces

--- spec/mycar_spec.rb
+++ spec/mycar_spec.rb
@@ -1,17 +1,16 @@
 RSpec.describe Car::MyCar do
+  it 'creates instance of MyCar class' do
+    volvo = Car::MyCar.new(2020, 'volvo', 'black')
+    expect(volvo).to be_instance_of(Car::MyCar)
+  end
 
-    it "creates instanse of MyCar class" do
-        volvo = Car::MyCar.new(2020, "volvo", "black")
-        expect(volvo).to be_instanse_of(Car::MyCar)
-    end
+  it 'the car speed is 0' do
+    volvo = Car::MyCar.new(2020, 'volvo', 'black')
+    expect(volvo.car_speed).to eq(0)
+  end
 
-    it "the car speed is 0" do
-        volvo = Car::MyCar.new(2020, "volvo", "black")
-        expect(volvo.car_speed).to eq(0)
-    end
-
-    it "speed up" do
-        volvo = Car::MyCar.new(2020, "volvo", "black")
-        expect(volvo.speed_up(10)).to be > 0
-    end
+  it 'speed up' do
+    volvo = Car::MyCar.new(2020, 'volvo', 'black')
+    expect(volvo.speed_up(10)).to be.positive?
+  end
 end
