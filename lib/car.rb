1d386e2ab67b9176aba378b0c60a647d533a2a61
nikolatriki <kjofte@yahoo.com>
2020-03-05 22:16:01 +0100

    new methods for defect and service info and tests

--- lib/car.rb
+++ lib/car.rb
@@ -5,13 +5,14 @@ module Car
     SPEED_OFFSET = 10
 
     attr_accessor :color, :car_speed
-    attr_reader :year
+    attr_reader :year, :defects
 
     def initialize(year, model, color)
       @year = year
       @model = model
       @color = color
       @car_speed = 0
+      @defects = []
     end
 
     def speed_up(increase = SPEED_OFFSET)
@@ -20,6 +21,19 @@ module Car
 
     def slow_down(decrease = SPEED_OFFSET)
       @car_speed -= decrease
+    end
+
+    def spray_paint(arg)
+      @color = arg
+    end
+
+    def add_defect(arg)
+      @defects.push(arg)
+    end
+
+    def service_info
+      defects = @defects.join(', ')
+      "Detected defects: #{defects}"
     end
   end
 end
--- spec/mycar_spec.rb
+++ spec/mycar_spec.rb
@@ -1,36 +1,52 @@
 RSpec.describe Car::MyCar do
+  let(:volvo) { Car::MyCar.new(2020, 'volvo', 'black') }
+
   it 'creates instance of MyCar class' do
-    volvo = Car::MyCar.new(2020, 'volvo', 'black')
     expect(volvo).to be_instance_of(Car::MyCar)
   end
 
   it 'the car speed is 0' do
-    volvo = Car::MyCar.new(2020, 'volvo', 'black')
     expect(volvo.car_speed).to eq(0)
   end
 
   it 'speed up by default' do
-    volvo = Car::MyCar.new(2020, 'volvo', 'black')
     volvo.speed_up
     expect(volvo.car_speed).to eq(Car::MyCar::SPEED_OFFSET)
   end
 
   it 'speed even up' do
-    volvo = Car::MyCar.new(2020, 'volvo', 'black')
     volvo.speed_up(60)
     expect(volvo.car_speed).to be > 59
   end
 
   it 'slows down' do
-    volvo = Car::MyCar.new(2020, 'volvo', 'black')
     volvo.slow_down
     expect(volvo.car_speed).to eq(-Car::MyCar::SPEED_OFFSET)
   end
 
   it 'slows down from some speed' do
-    volvo = Car::MyCar.new(2020, 'volvo', 'black')
     volvo.speed_up
     volvo.slow_down(10)
     expect(volvo.car_speed).to be(0)
+  end
+
+  it 'changes the color of the car' do
+    volvo.spray_paint :yellow
+    expect(volvo.color).to eq(:yellow)
+  end
+
+  it 'has no defects' do
+    expect(volvo.defects).to be_empty
+  end
+
+  it "add_defects" do
+    volvo.add_defect('water pump failure')
+    expect(volvo.defects).to include('water pump failure')
+  end
+
+  it 'list all the defects' do
+    volvo.add_defect('water pump failure')
+    volvo.add_defect('oil leak')
+    expect(volvo.service_info).to eq('Detected defects: water pump failure, oil leak')
   end
 end
